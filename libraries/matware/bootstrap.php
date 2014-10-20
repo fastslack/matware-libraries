a0ee2e897a91553ffd41bbd617040ac190934b66
Matias Aguirre <maguirre@matware.com.ar>
2014-10-20 01:03:25 -0300

    Load websocket libraries too

--- libraries/matware/bootstrap.php
+++ libraries/matware/bootstrap.php
@@ -14,3 +14,17 @@ defined('JPATH_PLATFORM') or die;
 
 // Register the classes for autoload.
 JLoader::registerPrefix('M', JPATH_LIBRARIES . '/matware');
+
+// Import the MWebSocket class from Matware libraries.
+JLoader::register('MSocketDaemon', JPATH_LIBRARIES.'/matware/socket/daemon.php');
+JLoader::register('MWebsocket', JPATH_LIBRARIES.'/matware/socket/websocket.php');
+JLoader::register('MWebsocketUser', JPATH_LIBRARIES.'/matware/socket/websocket/user.php');
+
+// Interfaces.
+JLoader::register('MWebsocketController', JPATH_LIBRARIES.'/matware/socket/websocket/controller.php');
+JLoader::register('MWebsocketHandshake', JPATH_LIBRARIES.'/matware/socket/websocket/handshake.php');
+JLoader::register('MWebsocketProtocol', JPATH_LIBRARIES.'/matware/socket/websocket/protocol.php');
+
+// Adapters.
+JLoader::discover('MWebsocketHandshake', JPATH_LIBRARIES.'/matware/socket/websocket/handshakes');
+JLoader::discover('MWebsocketProtocol', JPATH_LIBRARIES.'/matware/socket/websocket/protocols');
