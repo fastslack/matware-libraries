dc8e24647a943a92ee277d31e3732d01905f61a4
klas <klas.berlic@gmail.com>
2015-11-12 13:59:27 +0100

    fix broken menu nesting and improve categories consistency

--- libraries/matware/jupgrade/schemas/1.5/categories.php
+++ libraries/matware/jupgrade/schemas/1.5/categories.php
@@ -1,15 +1,15 @@
 <?php
 /**
-* jUpgradePro
-*
-* @version $Id:
-* @package jUpgradePro
-* @copyright Copyright (C) 2004 - 2014 Matware. All rights reserved.
-* @author Matias Aguirre
-* @email maguirre@matware.com.ar
-* @link http://www.matware.com.ar/
-* @license GNU General Public License version 2 or later; see LICENSE
-*/
+ * jUpgradePro
+ *
+ * @version $Id:
+ * @package jUpgradePro
+ * @copyright Copyright (C) 2004 - 2014 Matware. All rights reserved.
+ * @author Matias Aguirre
+ * @email maguirre@matware.com.ar
+ * @link http://www.matware.com.ar/
+ * @license GNU General Public License version 2 or later; see LICENSE
+ */
 
 // Require the category class
 JLoader::register("JUpgradeproCategory", JPATH_LIBRARIES."/matware/jupgrade/category.php");
@@ -154,7 +154,7 @@ class JUpgradeproCategories extends JUpgradeproCategory
 			}
 
 			// Reset some fields
-			$category['asset_id'] = $category['lft'] = $category['rgt'] = null;
+			$category['asset_id'] = $category['lft'] = $category['rgt'] =  null;
 			// Check if path is correct
 			$category['path'] = empty($category['path']) ? $category['alias'] : $category['path'];
 			// Fix the access
@@ -186,6 +186,16 @@ class JUpgradeproCategories extends JUpgradeproCategory
 
 			// Updating the steps table
 			$this->_step->_nextID($total);
+		}
+
+		// rebuild table
+
+		$category = JTable::getInstance('Category', 'JTable', array('dbo' => $this->_db));
+
+		try {
+			$category->rebuild();
+		} catch (RuntimeException $e) {
+			throw new RuntimeException($e->getMessage());
 		}
 
 		return false;
